6188926a64cf79f01b8a3b67112cae19b9474b65
anabeatrizfreitass <anabeatrizfreitas771@gmail.com>
2024-09-12 12:06:03 -0300

    Create notamedia1.py

---
+++ notamedia1.py
@@ -0,0 +1,9 @@
+#Feito pelas alunas Eluane, Ana Beatriz, Ana Letícia e Guilherme Delu
+nota = int ( input ( 'Digite uma nota: ' ))
+nota2 = int(input('Digite uma nota2: '))
+nota3 = int(input('Digite uma nota3: '))
+nota4 = int(input('Digite uma nota4: '))
+imprimir(nota, nota2, nota3, nota4)
+mídia = (nota + nota2 + nota3 + nota4) / 4
+mídia1 = (nota/4 + nota2/4 + nota3/4 + nota4/4)
+imprimir(mídia,mídia1)
